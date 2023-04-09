1e1faaa3a700aa9ce8d45e0ecc0694357eb098b9
stormstina <stina.nerin@gmail.com>
2023-04-09 15:31:30 +0200

    Function to add user rting to api

--- frontend/js/rating.js
+++ frontend/js/rating.js
@@ -5,16 +5,43 @@ const rating = (wrapper) => {
             const labels = [...arr].map(star => star.parentElement)
             removeClass(labels, "active");
             labels.slice(0, index + 1).forEach(star => addClass([star], "active"))
-            //todo! Add value to api
-            addRating(star.value)
+            addRating(star.value, wrapper.dataset.id)
         })
-        )
-    }
+    )
+}
     
-const addRating = async(rating) => {
+const addRating = async(newRating, bookId) => {
+    try {
+        console.log("rating", newRating, "bookID", bookId);
+        const res = await axios.get(`http://localhost:1337/api/books/${bookId}?populate=*`)
+        const arr = res.data.data.attributes.rating;
+        console.log("pre push", arr);
+        arr.push({rating: newRating})
+        console.log("after push", arr);
+        updateRating(arr, bookId)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const updateRating = async(arr, bookId) => {
+    try {
+        const res = await axios.put(`http://localhost:1337/api/books/${bookId}?populate=*`,
+            {
+                data: {
+                    rating: arr
+                }
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+                    "Content-Type": "application/json"
+                }
+            },
+        )
+        console.log(res.data.data.attributes.rating);
         
-    console.log(rating);
-
-    const res = await axios.post("")
-
+    } catch (error) {
+        console.log(error);
+    }
 }
